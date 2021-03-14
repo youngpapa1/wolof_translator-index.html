22e4eae68fff020eff279b371f9c2755c6984f23
lameeno <56945844+ne18070@users.noreply.github.com>
2021-03-14 17:35:15 +0900

    Update application.js

--- application.js
+++ application.js
@@ -4,16 +4,9 @@ var outputText = document.querySelector("#output-text");
 
 
 function getTranslationUrl(text){
-    return {
-        "success": {
-            "total": 1
-        },
-        "contents": {
-            "translated": "\"\"",
-            "text": "\"\"",
-            "translation": "naga def"
-        }
-    }
+    translated = "naga def"
+    outputText.innerText = translated
+    return;
 }
 
 function errorHandler(error){
@@ -23,14 +16,7 @@ function clickEventHandler(){
     // outputText.innerText=inputText.value;
 
     var inputTxt = inputText.value;
-    fetch(getTranslationUrl(inputTxt))
-    .then(response => response.json())
-    .then(json=>{
-        var translated = json.contents.translated;
-        outputText.innerText = translated;
-    }
-    )
-    .catch(errorHandler)
+    getTranslationUrl(inputTxt);
 
 }
 btnTranslate.addEventListener("click",clickEventHandler)
