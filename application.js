09dea1a4a54f9659ebd26a7673e432a6c185fc93
lameeno <56945844+ne18070@users.noreply.github.com>
2021-03-14 18:46:18 +0900

    Update application.js

--- application.js
+++ application.js
@@ -4,12 +4,15 @@ var outputText = document.querySelector("#output-text");
 
 
 function getTranslationUrl(text){
-  if (text =="bonjour" || text =="Bonjour" || text =="Hello" || text =="こんにちは" ){
-    outputText.innerText = "naga def"
-  }else{
-    outputText.innerText = "We try.	On essaye"
+  outputText.innerTex ="";
+  for(var i = 0; i < words.length; i++)
+  {
+    if(words[i].word.fr == text)
+    {
+      outputText.innerText = words[i].word.wo;
+      return;
+    }
   }
-    return;
 }
 
 function errorHandler(error){
@@ -23,3 +26,20 @@ function clickEventHandler(){
 
 }
 btnTranslate.addEventListener("click",clickEventHandler)
+
+words =
+  [
+    {"word" : { "fr" : "bonjour", "wo" : "naga def" }},
+    {"word" : { "fr" : "hello", "wo" : "naga def" }},
+    {"word" : { "fr" : "こんにちは", "wo" : "naga def" }},
+    {"word" : { "fr" : "un", "wo" : "benn" }},
+    {"word" : { "fr" : "deux", "wo" : "ñaar" }},
+    {"word" : { "fr" : "trois", "wo" : "ñett" }},
+    {"word" : { "fr" : "quatre", "wo" : "ñeent" }},
+    {"word" : { "fr" : "cinq", "wo" : "juróom" }},
+    {"word" : { "fr" : "six", "wo" : "juróom-benn" }},
+    {"word" : { "fr" : "sept", "wo" : "juróom-ñaar" }},
+    {"word" : { "fr" : "huit", "wo" : "juróom-ñett" }},
+    {"word" : { "fr" : "neuf", "wo" : "juróom-ñeent" }},
+    {"word" : { "fr" : "dix", "wo" : "fukk" }}
+];
